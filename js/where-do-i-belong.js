68d696fabbfb2172cdb482f2275e91a456a9ed45
Andrea Sonny <andreasonny83@gmail.com>
2016-05-28 20:53:41 +0100

    where-do-i-belong improved

--- js/where-do-i-belong.js
+++ js/where-do-i-belong.js
@@ -3,30 +3,19 @@
  * should be inserted into an array (first argument) once it has been sorted.
  */
 function getIndexToIns(arr, value) {
-  var pos = 0;
   var i = 0;
-  var sortedArr = arr.sort(function(a, b) {
+
+  arr.sort(function(a, b) {
     return a - b;
   });
 
-  for (i = 0; i < sortedArr.length; i++) {
-    if (sortedArr[0] >= value) {
-      pos = 0;
-      break;
-    }
-
-    if (value <= sortedArr[i + 1]) {
-      pos = i + 1;
-      break;
-    }
-
-    if (i + 1 === sortedArr.length) {
-      pos = sortedArr.length;
-      break;
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] >= value) {
+      return i;
     }
   }
 
-  return pos;
+  return arr.length;
 }
 
 module.exports = getIndexToIns;
